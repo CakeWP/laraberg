41aea3b074acb7745a55e3b73041cfb1e5f272a6
ZafarKamal123 <sk4915497@gmail.com>
2022-08-20 14:32:48 +0500

    :+1: point to local fork block-editor repository

--- resources/ts/laraberg.ts
+++ resources/ts/laraberg.ts
@@ -1,4 +1,4 @@
 import '@van-ons/block-editor/dist/styles.css'
 
-export { wordpress, Editor, registerBlockType, removeEditor } from '@van-ons/block-editor'
+export { wordpress, Editor, registerBlockType, removeEditor, fills } from '@van-ons/block-editor'
 export { init } from './init'
